c3c54f23d177d847f7ed8af9aa265dd5e16131d8
Adam Casey <adam.jw.casey@gmail.com>
2024-02-05 19:31:32 -0700

    Change highlighted item style

--- sniper/src/view.rs
+++ sniper/src/view.rs
@@ -11,8 +11,7 @@ pub fn view(model: &mut Sniper, f: &mut Frame) {
     f.render_stateful_widget(
         List::new(model.file_list.files.clone())
             .block(Block::default().title("Files").borders(Borders::ALL))
-            .highlight_style(Style::default().add_modifier(Modifier::ITALIC))
-            .highlight_symbol(">>")
+            .highlight_style(Style::default().add_modifier(Modifier::REVERSED))
             .repeat_highlight_symbol(true),
         f.size(),
         &mut model.file_list.state,
